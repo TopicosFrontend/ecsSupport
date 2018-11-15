2d6b91f1fbc342d4a67907900b4aef552c163159
esilvac <esilva1995@gmail.com>
2018-11-15 11:22:30 -0500

    Delete example data

--- src/app/show-form/show-form.component.ts
+++ src/app/show-form/show-form.component.ts
@@ -16,32 +16,6 @@ export class ShowFormComponent implements OnInit {
 
   response_accepted = false
   response = {}
-  res = {
-    "codigo": {"cfn": "2-1", "ecn": "Q"},
-    "secciones": [
-      {
-          "preguntas": [{ "respuesta": "answ 1" }, { "respuesta": "answ 2" }]
-      },
-      {
-          "preguntas": [{ "respuesta": "answ 3" }, { "respuesta": "answ 4" }, { "respuesta": "answ 5" }]
-      },
-      {
-          "preguntas": [{ "respuesta": "answ 6" }, { "respuesta": "answ 7" }]
-      },
-      {
-          "preguntas": [{ "respuesta": "answ 8" }, { "respuesta": "answ 9" }, { "respuesta": "answ 10" }, { "respuesta": "answ 11" }]
-      },
-      {
-          "preguntas": [{ "respuesta": "answ 12" }, { "respuesta": "answ 13" }, { "respuesta": "answ 14" }]
-      },
-      {
-          "preguntas": [{ "respuesta": "answ 15" }, { "respuesta": "answ 16" }, { "respuesta": "answ 17" }]
-      },
-      {
-          "preguntas": [{ "respuesta": "answ 18" }, { "respuesta": "answ 19" }, { "respuesta": "answ 20" }]
-      }
-    ]
-  }
 
   constructor(public provider_support: ProviderSupportService, private router: Router) { }
 
@@ -52,7 +26,6 @@ export class ShowFormComponent implements OnInit {
     if (this.cfn != '' && this.ecn != '') {
       this.provider_support.show_form(this.ecn, this.cfn).subscribe(response => {
         this.response = response.form
-        this.response = this.res
         this.response_accepted = true
       });
     } else {
