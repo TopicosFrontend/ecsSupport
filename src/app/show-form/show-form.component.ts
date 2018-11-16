affc355790d2ba1477d0d42db4c43c361ca4ee2c
esilvac <esilva1995@gmail.com>
2018-11-16 01:39:10 -0500

    Fix show form

--- src/app/show-form/show-form.component.ts
+++ src/app/show-form/show-form.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProviderSupportService } from '../provider-services/provider-support.service';
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import FormData from '../../assets/form/data.json';
-
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-show-form',
   templateUrl: './show-form.component.html',
-  styleUrls: ['./show-form.component.css']
+  styleUrls: ['./show-form.component.scss']
 })
+
 export class ShowFormComponent implements OnInit {
-  ecn = ''
-  cfn = ''
   data = FormData
 
-  response_accepted = false
+  cfn = ''
+  ecn = ''
   response = {}
+  response_accepted = false
+
+  faAngleDown = faAngleDown;
+  faAngleUp = faAngleUp;
 
   constructor(public provider_support: ProviderSupportService, private router: Router) { }
 
@@ -25,7 +30,8 @@ export class ShowFormComponent implements OnInit {
   show_form() {
     if (this.cfn != '' && this.ecn != '') {
       this.provider_support.show_form(this.cfn, this.ecn).subscribe(response => {
-        if (!response["state"]) {
+        console.log(response)
+        if (response["state"]) {
           this.response = response
           this.response_accepted = true
         } else {
@@ -35,6 +41,12 @@ export class ShowFormComponent implements OnInit {
     } else {
       alert("Copia los codigos ECN y CFN");
     }
+  }
+
+  toggle_subject(id) {
+    $("#section-" + id).toggle();
+    $("#section-info-" + id + " .arrow-up").toggle();
+    $("#section-info-" + id + " .arrow-down").toggle();
   }
 
   lobby() {
